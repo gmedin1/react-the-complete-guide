ddbebf1e2a2b62752aa65572130fdfd197f36f9c
German Medina <germedin1@publicisgroupe.net>
2022-01-20 14:25:01 -0500

    assignment 2

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,15 +1,28 @@
+import { useState } from "react";
 import "./Expenses.css";
 import ExpenseItem from "./ExpenseItem";
+import ExpensesFilter from "./ExpensesFilter";
 import Card from "../UI/Card";
 
 function Expenses(props) {
+  const [filteredYear, setFilteredYear] = useState("2022");
+  const applyFilterHandler = (year) => {
+    setFilteredYear(year);
+  };
+
   return (
-    <Card className="expenses">
-      <ExpenseItem expense={props.expenses[0]} />
-      <ExpenseItem expense={props.expenses[1]} />
-      <ExpenseItem expense={props.expenses[2]} />
-      <ExpenseItem expense={props.expenses[3]} />
-    </Card>
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          onApplyFilter={applyFilterHandler}
+          defaultYear={filteredYear}
+        />
+        <ExpenseItem expense={props.expenses[0]} />
+        <ExpenseItem expense={props.expenses[1]} />
+        <ExpenseItem expense={props.expenses[2]} />
+        <ExpenseItem expense={props.expenses[3]} />
+      </Card>
+    </div>
   );
 }
 
