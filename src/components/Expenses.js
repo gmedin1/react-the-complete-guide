f8df5ceaef90cee7dbdb81a060e03d3d9a8e847f
German Medina <germedin1@publicisgroupe.net>
2022-01-20 10:02:56 -0500

    assignment 1

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -4,14 +4,10 @@ import ExpenseItem from "./ExpenseItem";
 function Expenses(props) {
   return (
     <div className="expenses">
-      {props.expenses.map((expense) => (
-        <ExpenseItem
-          key={expense.id}
-          date={expense.date}
-          title={expense.title}
-          amount={expense.amount}
-        />
-      ))}
+      <ExpenseItem expense={props.expenses[0]} />
+      <ExpenseItem expense={props.expenses[1]} />
+      <ExpenseItem expense={props.expenses[2]} />
+      <ExpenseItem expense={props.expenses[3]} />
     </div>
   );
 }
