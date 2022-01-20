7935ff2bf631f22d4f139c60ac6e2b572a6c365d
German Medina <germedin1@publicisgroupe.net>
2022-01-20 14:06:39 -0500

    assignment 1

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,16 @@
+import "./Expenses.css";
+import ExpenseItem from "./ExpenseItem";
+import Card from "../UI/Card";
+
+function Expenses(props) {
+  return (
+    <Card className="expenses">
+      <ExpenseItem expense={props.expenses[0]} />
+      <ExpenseItem expense={props.expenses[1]} />
+      <ExpenseItem expense={props.expenses[2]} />
+      <ExpenseItem expense={props.expenses[3]} />
+    </Card>
+  );
+}
+
+export default Expenses;
---
+++ src/components/NewExpense/NewExpense.js
@@ -0,0 +1,23 @@
+import "./NewExpense.css";
+import ExpenseForm from "./ExpenseForm";
+
+function NewExpense(props) {
+  const saveExpenseDataHandler = (expenseData) => {
+    const expense = {
+      ...expenseData,
+      id: Math.random().toString(),
+    };
+
+    props.addExpenseHandler(expense);
+  };
+
+  return (
+    <div>
+      <div className="new-expense">
+        <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+      </div>
+    </div>
+  );
+}
+
+export default NewExpense;
